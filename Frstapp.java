42d32a376a936f39903f779250a2ab8bc911a74a
sharadhiam <45512996+sharadhiam@users.noreply.github.com>
2019-02-14 23:08:36 +0530

    Add files via upload

---
+++ Frstapp.java
@@ -0,0 +1,14 @@
+package hello;
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Frstapp extends Applet
+{
+
+	public void paint(Graphics g)
+	{
+		// TODO Auto-generated method stub
+		g.drawString("Hello World!",150,150);
+	}
+
+}
